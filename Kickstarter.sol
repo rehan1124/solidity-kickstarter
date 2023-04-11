b2d513b6fbe68eee417015b022f4644d9749caaf
srehan1 <syed.rehan@shoppertrak.com>
2023-04-11 20:04:58 +0530

    Added Approvers and Contribute functionality

--- Kickstarter.sol
+++ Kickstarter.sol
@@ -5,9 +5,18 @@ pragma solidity ^0.8.18;
 contract Campaign {
     address public manager;
     uint256 public minimumContribution;
+    address[] public approvers;
 
     constructor(uint256 minAmount) {
         manager = msg.sender;
         minimumContribution = minAmount;
+    }
+
+    function contribute() public payable {
+        require(
+            msg.value > minimumContribution,
+            "Amount trying to send is less than minimum contribution for Campaign."
+        );
+        approvers.push(msg.sender);
     }
 }
